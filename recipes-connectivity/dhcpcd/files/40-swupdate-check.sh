979774214cb01e3197f846c5f46e880a9f9e0939
Reto Schneider <reto.schneider@husqvarnagroup.com>
2019-05-03 18:51:33 +0200

    SG-13163 swupdate/dhcpcd: Trigger swupdate on new DHCP leases

--- recipes-connectivity/dhcpcd/files/40-swupdate-check.sh
+++ recipes-connectivity/dhcpcd/files/40-swupdate-check.sh
@@ -3,12 +3,24 @@
 # if_up and interface are provided by dhcpcd
 # shellcheck disable=SC2154
 
-# Attempt update when connected to a new network
+# dhcpcd-run-hooks reasons we are dealing with:
+#
+# BOUND/BOUND6: dhcpcd obtained a new lease from a DHCP server.
+#               Works for all networks with a DHCP server. Very common
+#               nowadays, but we require it to exist only when using IPv4
+#               connectivity. IPv6 only networks might not have one.
+#               Considering this limitation as acceptable because this update-on-brief-
+#               disconnect is used mainly by developers.
+#
+# Not reacting on CARRIER because it is not guaranteed that the IP connectivity
+# is established by the time SWUpdate gets executed.
 
-if [ "${if_up}" = "true" ]; then
-    case "${interface}" in
-        eth0 | wlan0)
-            systemctl restart swupdate-check.timer || true
-            ;;
-    esac
-fi
+case "${reason}" in
+    BOUND | BOUND6)
+        case "${interface}" in
+            eth0 | wlan0)
+                systemctl restart swupdate-check.timer || true
+                ;;
+        esac
+        ;;
+esac
