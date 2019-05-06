68d84d38318c9128953870ea10f639e6bf9425bc
Reto Schneider <reto.schneider@husqvarnagroup.com>
2019-05-06 18:01:39 +0200

    SG-13163 swupdate/dhcpcd: Remove no longer relevant comment

--- recipes-connectivity/dhcpcd/files/40-swupdate-check.sh
+++ recipes-connectivity/dhcpcd/files/40-swupdate-check.sh
@@ -6,23 +6,8 @@
 # Best-effort solution to trigger an update attempt when the gateway gets
 # connected to a network.
 #
-# dhcpcd-run-hooks reasons we want to deal with:
-#
-# BOUND/BOUND6: dhcpcd obtained a new lease from a DHCP server.
-#               Works for all networks with a DHCP server. Very common
-#               nowadays, but we require it to exist only when using IPv4
-#               connectivity. IPv6 only networks might not have one.
-# ROUTERADVERT: dhcpcd has received an IPv6 router advertisement, or one has
-#               expired.
-#               Received regularly in IPv6 networks even when DHCP is not used.
-#               We must not trigger an update attempt for every message, but
-#               just one single time after a carrier has come back up.
-#               Please note that this script does not trigger an update attempt
-#               when a new route has been advertised.
-# NOCARRIER:    When received, causes the next ROUTERADVERT to trigger an
-#               update.
-# CARRIER:      Not being used because it is not guaranteed that the IP
-#               connectivity is established by the time SWUpdate gets executed.
+# We are not simply using CARRIER because then we could not guaranteed that the
+# IP connectivity is established by the time SWUpdate gets executed.
 
 update_marker="/var/volatile/dhcpcd-swupdate-triggered"
 
