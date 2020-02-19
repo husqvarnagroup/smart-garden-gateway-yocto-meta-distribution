d678e3144b9786e8029582edbe37745098517894
Reto Schneider <reto.schneider@husqvarnagroup.com>
2020-02-19 09:41:53 +0100

    SG-15324 rsyslog: Make initialization script omnipotent
    * Script runs now on each startup and changes in the Seluxit environment
      do get reflected.
    * The rsyslog unit waits now for rsyslog-gw-init to be finished before
      it starts.
    * /var/spool/rsyslog is now in the base image
    * This also fixes a power cut vulnerability introduced in
      145f10931423f96f7e194e6b2914a1fe4345d72e.

--- recipes-extended/rsyslog/rsyslog/rsyslog-gw-init.sh
+++ recipes-extended/rsyslog/rsyslog/rsyslog-gw-init.sh
@@ -1,48 +1,45 @@
 #!/bin/sh
-# shellcheck disable=SC2154,SC2039
-#
 # Setup gateway configuration for rsyslog
+#
+# This script is not power cut save as it is supposed to run on each startup
+# before rsyslog gets started.
+# Running on reach start up ensures that environment changes are reflected
+# properly.
 
 set -eu -o pipefail
 
 RSYSLOG_CONFIG_DIR='/etc/rsyslog.d'
-RSYSLOG_SPOOL_DIR='/var/spool/rsyslog'
 SELUXIT_ENV="$(fw_printenv -n "seluxit_env" 2>/dev/null || echo prod)"
-
-mkdir -p ${RSYSLOG_SPOOL_DIR}
+GATEWAY_ID="$(fw_printenv -n gatewayid)"
 
 # Configure gateway id as LocalHostName
 RSYSLOG_CONFIG_FILE="${RSYSLOG_CONFIG_DIR}/01-gateway-id.conf"
-if [ ! -s "${RSYSLOG_CONFIG_FILE}" ]; then
-    mkdir -p ${RSYSLOG_CONFIG_DIR}
-    echo "\$LocalHostName $(fw_printenv -n gatewayid)" > "${RSYSLOG_CONFIG_FILE}.tmp"
-    sync
-    mv "${RSYSLOG_CONFIG_FILE}.tmp" "${RSYSLOG_CONFIG_FILE}"
-fi
+RSYSLOG_CONFIG_FILE_CONTENT="\$LocalHostName ${GATEWAY_ID}"
+grep -q ^"${RSYSLOG_CONFIG_FILE_CONTENT}"$ "${RSYSLOG_CONFIG_FILE}" 2>/dev/null || echo "${RSYSLOG_CONFIG_FILE_CONTENT}" > "${RSYSLOG_CONFIG_FILE}"
 
 # Configure variables for use in other config files
 RSYSLOG_METADATA_CONFIG_FILE="${RSYSLOG_CONFIG_DIR}/02-gateway-metadata.conf"
-if [ ! -s "${RSYSLOG_METADATA_CONFIG_FILE}" ]; then
-    mkdir -p ${RSYSLOG_CONFIG_DIR}
-    echo "set \$!gw.env = '${SELUXIT_ENV}';" >> "${RSYSLOG_METADATA_CONFIG_FILE}.tmp"
-    # shellcheck disable=SC1091
-    echo "set \$!gw.swVersion = '$(. /etc/os-release; echo "$VERSION")';" >> "${RSYSLOG_METADATA_CONFIG_FILE}.tmp"
-    sync
-    mv "${RSYSLOG_METADATA_CONFIG_FILE}.tmp" "${RSYSLOG_METADATA_CONFIG_FILE}"
+RSYSLOG_METADATA_CONFIG_FILE_CONTENT_ENV="set \$!gw.env = '${SELUXIT_ENV}';"
+# shellcheck disable=SC1091
+RSYSLOG_METADATA_CONFIG_FILE_CONTENT_VERSION="set \$!gw.swVersion = '$(. /etc/os-release; echo "$VERSION")';"
+if ! grep -q ^"${RSYSLOG_METADATA_CONFIG_FILE_CONTENT_ENV}"$ "${RSYSLOG_METADATA_CONFIG_FILE}" || \
+   ! grep -q ^"${RSYSLOG_METADATA_CONFIG_FILE_CONTENT_VERSION}"$ "${RSYSLOG_METADATA_CONFIG_FILE}"; then
+    echo "${RSYSLOG_METADATA_CONFIG_FILE_CONTENT_ENV}" > "${RSYSLOG_METADATA_CONFIG_FILE}"
+    echo "${RSYSLOG_METADATA_CONFIG_FILE_CONTENT_VERSION}" >> "${RSYSLOG_METADATA_CONFIG_FILE}"
 fi
 
 # Configure diagnostics based on environment
 DIAGNOSTICS_CONFIG_FILE="${RSYSLOG_CONFIG_DIR}/20-diagnostics.conf"
 if [ "${SELUXIT_ENV}" = prod ]; then
-    cmp "${DIAGNOSTICS_CONFIG_FILE}.prod" "${DIAGNOSTICS_CONFIG_FILE}" 2>/dev/null || cp "${DIAGNOSTICS_CONFIG_FILE}.prod" "${DIAGNOSTICS_CONFIG_FILE}"
+    cmp "${DIAGNOSTICS_CONFIG_FILE}.prod" "${DIAGNOSTICS_CONFIG_FILE}" || cp "${DIAGNOSTICS_CONFIG_FILE}.prod" "${DIAGNOSTICS_CONFIG_FILE}"
 else
-    cmp "${DIAGNOSTICS_CONFIG_FILE}.dev" "${DIAGNOSTICS_CONFIG_FILE}" 2>/dev/null || cp "${DIAGNOSTICS_CONFIG_FILE}.dev" "${DIAGNOSTICS_CONFIG_FILE}"
+    cmp "${DIAGNOSTICS_CONFIG_FILE}.dev" "${DIAGNOSTICS_CONFIG_FILE}" || cp "${DIAGNOSTICS_CONFIG_FILE}.dev" "${DIAGNOSTICS_CONFIG_FILE}"
 fi
 
 # Disable verbose logging on prod
 FILTER_CONFIG_FILE="${RSYSLOG_CONFIG_DIR}/90-severity-forward-filter.conf"
 if [ "${SELUXIT_ENV}" = prod ]; then
-    cmp "${FILTER_CONFIG_FILE}.prod" "${FILTER_CONFIG_FILE}" 2>/dev/null || cp "${FILTER_CONFIG_FILE}.prod" "${FILTER_CONFIG_FILE}"
+    cmp "${FILTER_CONFIG_FILE}.prod" "${FILTER_CONFIG_FILE}" || cp "${FILTER_CONFIG_FILE}.prod" "${FILTER_CONFIG_FILE}"
 else
     rm -f "${FILTER_CONFIG_FILE}"
 fi
