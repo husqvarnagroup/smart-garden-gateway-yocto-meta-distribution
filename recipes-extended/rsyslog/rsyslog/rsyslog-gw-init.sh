husqvarnagroup/smart-garden-gateway-yocto-meta-distribution

commit 02fa04e3754abed38d09f03a6e4ca8a59a6c3808

SG-15105 rsyslog: Fix empty gw.env variable in prod

recipes-extended/rsyslog/rsyslog/rsyslog-gw-init.sh
@@ -7,6 +7,7 @@ set -eu -o pipefail
 
 RSYSLOG_CONFIG_DIR='/etc/rsyslog.d'
 RSYSLOG_SPOOL_DIR='/var/spool/rsyslog'
+SELUXIT_ENV="$(fw_printenv -n "seluxit_env" 2>/dev/null || echo prod)"
 
 mkdir -p ${RSYSLOG_SPOOL_DIR}
 
@@ -23,7 +24,7 @@ fi
 RSYSLOG_METADATA_CONFIG_FILE="${RSYSLOG_CONFIG_DIR}/02-gateway-metadata.conf"
 if [ ! -s "${RSYSLOG_METADATA_CONFIG_FILE}" ]; then
     mkdir -p ${RSYSLOG_CONFIG_DIR}
-    echo "set \$!gw.env = '$(fw_printenv -n seluxit_env)';" >> "${RSYSLOG_METADATA_CONFIG_FILE}.tmp"
+    echo "set \$!gw.env = '$(SELUXIT_ENV)';" >> "${RSYSLOG_METADATA_CONFIG_FILE}.tmp"
     # shellcheck disable=SC1091
     echo "set \$!gw.swVersion = '$(. /etc/os-release; echo "$VERSION")';" >> "${RSYSLOG_METADATA_CONFIG_FILE}.tmp"
     sync
@@ -33,7 +34,6 @@ fi
 # Disable verbose logging on prod
 FILTER_CONFIG_FILE="${RSYSLOG_CONFIG_DIR}/90-severity-forward-filter.conf"
 RATELIMIT_CONFIG_FILE="${RSYSLOG_CONFIG_DIR}/90-rate-limit.conf"
-SELUXIT_ENV="$(fw_printenv -n "seluxit_env" 2>/dev/null || echo prod)"
 if [ "${SELUXIT_ENV}" = prod ]; then
     cmp "${FILTER_CONFIG_FILE}.prod" "${FILTER_CONFIG_FILE}" 2>/dev/null || cp "${FILTER_CONFIG_FILE}.prod" "${FILTER_CONFIG_FILE}"
     cmp "${RATELIMIT_CONFIG_FILE}.prod" "${RATELIMIT_CONFIG_FILE}" 2>/dev/null || cp "${RATELIMIT_CONFIG_FILE}.prod" "${RATELIMIT_CONFIG_FILE}"

commit fa02714ad7e76db469223332b98b0269028eb70e

SG-15325 rsyslog: Fix environment reporting
This error got introduced in 02fa04e3754abed38d09f03a6e4ca8a59a6c3808.

recipes-extended/rsyslog/rsyslog/rsyslog-gw-init.sh
@@ -24,7 +24,7 @@ fi
 RSYSLOG_METADATA_CONFIG_FILE="${RSYSLOG_CONFIG_DIR}/02-gateway-metadata.conf"
 if [ ! -s "${RSYSLOG_METADATA_CONFIG_FILE}" ]; then
     mkdir -p ${RSYSLOG_CONFIG_DIR}
-    echo "set \$!gw.env = '$(SELUXIT_ENV)';" >> "${RSYSLOG_METADATA_CONFIG_FILE}.tmp"
+    echo "set \$!gw.env = '${SELUXIT_ENV}';" >> "${RSYSLOG_METADATA_CONFIG_FILE}.tmp"
     # shellcheck disable=SC1091
     echo "set \$!gw.swVersion = '$(. /etc/os-release; echo "$VERSION")';" >> "${RSYSLOG_METADATA_CONFIG_FILE}.tmp"
     sync

recipes-extended/rsyslog/rsyslog_%.bbappend
@@ -1,6 +1,6 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
 
-PR_append = ".9"
+PR_append = ".10"
 
 DEPENDS += "openssl"
 RDEPENDS_${PN} += "ca-certificates environment"