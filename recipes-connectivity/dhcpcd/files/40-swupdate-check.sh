a42fc972ac3ee72ce356ca2cbc23865983f60fd2
Lukas Woodtli <lukas.woodtli@husqvarnagroup.com>
2022-10-19 08:58:18 +0200

    SG-19233 swupdate/dhcpcd: Reduce checks for update on new network connection
    To reduce excessive checks for SW updates and the resulting cost
    the check for updates on new network lease is only done until an
    update image is fetched.

--- recipes-connectivity/dhcpcd/files/40-swupdate-check.sh
+++ recipes-connectivity/dhcpcd/files/40-swupdate-check.sh
@@ -1,29 +1,30 @@
 # shellcheck disable=SC2148
 # shellcheck shell=dash
-# if_up and interface are provided by dhcpcd
+# `interface` and `reason` are provided by dhcpcd
 # shellcheck disable=SC2154
 #
-# Best-effort solution to trigger an update attempt when the gateway gets
-# connected to a network.
+# Trigger an update attempt when the gateway gets connected to a network.
 #
-# We are not simply using CARRIER because then we could not guaranteed that the
+# Skip the update check if there is a marker file. That file is created
+# by the Lua script embedded in the update image. As the marker file
+# is located in `/tmp` it's removed when the GW is rebooted (e.g. after a
+# successful update).
+#
+# We are not simply using CARRIER because then we could not guarantee that the
 # IP connectivity is established by the time SWUpdate gets executed.
 
-update_marker="/var/volatile/dhcpcd-swupdate-triggered"
+update_marker="/tmp/dhcp_fw_update_check_delay"
 
 try_update()
 {
     [ -e "${update_marker}" ] && return 0
 
     systemctl restart swupdate-check.timer
-    touch "${update_marker}"
 }
 
 # The only interesting interfaces are Ethernet and WiFi
 if [ "${interface}" = eth0 ] || [ "${interface}" = wlan0 ]; then
-    if [ "${if_down}" = true ]; then
-        rm -f "${update_marker}"
-    elif [ "${reason}" = BOUND ] || [ "${reason}" = BOUND6 ] || [ "${reason}" = ROUTERADVERT ]; then
+    if [ "${reason}" = BOUND ] || [ "${reason}" = BOUND6 ] || [ "${reason}" = ROUTERADVERT ]; then
         try_update
     fi
 fi
