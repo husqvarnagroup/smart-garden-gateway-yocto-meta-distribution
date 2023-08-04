f4851443df3c79c669eb0d8d376df8931d121eaf
Ezra Buehler <ezra.buehler@husqvarnagroup.com>
2023-08-05 00:05:33 +0200

    environment: Add support for essential U-Boot variables
    This is needed because the manufacturing-tools package is no longer
    part of the update image.
    
    Issue: SG-20495

--- recipes-support/environment/environment/environment.sh
+++ recipes-support/environment/environment/environment.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 #
-# Extract X.509 client certificate and key from U-Boot.
+# 1) Extract manufacturing step indicators from U-Boot and store them in the
+#    file system. This allows systemd depending on the environment.service
+#    units to make use of ConditionPathExists.
+#
+# 2) Extract X.509 client certificate and key from U-Boot.
 #
 # Since certificates files contain newlines and U-Boot variables can not contain
 # such, newlines are encoded as the string "%".
@@ -8,6 +12,17 @@
 # Precondition: The directories /etc/ssl/{certs,private} have to exist
 
 set -eu -o pipefail
+
+for var in fct_finalized eol_test_passed; do
+    status="$(fw_printenv -n ${var} 2>/dev/null || echo 0)"
+    if [ "${status}" = "1" ] || [ "$(uname -m)" = "armv5tejl" ]; then
+        file="/etc/${var}"
+        if [ ! -f ${file} ]; then
+            touch ${file}
+            sync
+        fi
+    fi
+done
 
 ssl_dir="/etc/ssl"
     
