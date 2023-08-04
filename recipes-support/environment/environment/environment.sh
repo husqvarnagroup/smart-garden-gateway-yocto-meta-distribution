e59c6bf3b1f09cdf670fcf9a51339de30ccd7e9f
Ezra Buehler <ezra.buehler@husqvarnagroup.com>
2023-08-04 19:17:25 +0200

    environment: Add support for new x509_* U-Boot variables
    Freshly produced gateways now come with x509_crt and x509_key set in
    place of the old conf_openvpn_* U-Boot variables.
    
    Issue: SG-20540

--- recipes-support/environment/environment/environment.sh
+++ recipes-support/environment/environment/environment.sh
@@ -25,7 +25,6 @@ for var in fct_finalized eol_test_passed; do
 done
 
 ssl_dir="/etc/ssl"
-    
 for ext in crt key; do
     if [ "${ext}" = crt ]; then
         file="${ssl_dir}/certs/client-prod.${ext}"
@@ -33,7 +32,6 @@ for ext in crt key; do
         file="${ssl_dir}/private/client-prod.${ext}"
     fi
 
-    uboot_var="conf_openvpn_${ext}"
     if [ -s "${file}" ]; then
         echo "File '${file}' already exists and is not empty"
         if [ "${ext}" = "key" ] && [ "$(stat -c "%a" "${file}")" != "600" ]; then
@@ -41,7 +39,10 @@ for ext in crt key; do
         fi
         continue
     fi
-    if content="$(fw_printenv -n "${uboot_var}" 2>/dev/null)"; then
+
+    if content="$(fw_printenv -n "x509_${ext}" 2>/dev/null)" ||
+        content="$(fw_printenv -n "conf_openvpn_${ext}" 2>/dev/null)"
+    then
         echo "${content}" | tr '%' '\n' > "${file}".tmp
         if [ "${ext}" = "key" ]; then
             chmod 600 "${file}".tmp
@@ -49,7 +50,7 @@ for ext in crt key; do
         sync
         mv "${file}".tmp "${file}"
     else
-        echo "U-Boot variable '${uboot_var}' is missing!" >&2
+        echo "U-Boot variable 'x509_${ext}' is missing!" >&2
         exit 1
     fi
 done
